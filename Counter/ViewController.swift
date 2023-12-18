e9e0ffb1b29da0fdbce371072b0a21f40b6656cd
a.n.lazarev <lazarev01@list.ru>
2023-12-18 22:48:58 +0500

    Добавил задачу 3 спринта в файл ViewController

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,59 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var counter: UILabel!
+    
+    @IBOutlet weak var history: UITextView!
+    
+    @IBOutlet weak var buttonIncrease: UIButton!
+    
+    @IBOutlet weak var buttonDecrease: UIButton!
+    
+    @IBOutlet weak var reset: UIButton!
+    
+    var number = 0
+    
+    let formatter = DateFormatter()
 
+    var counterText = "Значение счетчика: "
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        formatter.dateFormat = "yyyy/MM/dd HH:mm:ss"
+        history.text = "История изменений: \n\n"
+        history.isScrollEnabled = true
+        history.isEditable = false
         // Do any additional setup after loading the view.
     }
-
-
+    
+    func returnData() -> String {
+        let currentDate = formatter.string(from: Date())
+        return "\(currentDate): "
+    }
+    
+    @IBAction func clickIncreaseButton(_ sender: Any) {
+        number += 1
+        history.text.append(returnData() + "+1 \n")
+    }
+    
+    @IBAction func clickDecreaseButton(_ sender: Any) {
+        if number > 0 {
+            number -= 1
+            history.text.append(returnData() + "-1 \n")
+        } else {
+            history.text.append(returnData() + "попытка уменьшить значение счётчика ниже 0 \n")
+        }
+    }
+        
+    @IBAction func clickResetButton(_ sender: Any) {
+        number = 0
+        history.text.append(returnData() + "значение сброшено \n")
+    }
+        
+        
+    @IBAction func updateOnTap(_ sender: Any) {
+        counter.text = counterText + "\(number)"
+    }
 }
 
